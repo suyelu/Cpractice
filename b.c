10daa7bd23ce1009341e68a619610097df4af2fa
suyelu <suyelu@haizeix.com>
2019-07-21 14:58:54 +0800

    add b.c

---
+++ b.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: b.c
+	> Author: suyelu
+	> Mail: suyelu@haizeix.com
+	> Created Time: Sun 21 Jul 2019 02:56:31 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main() {
+    
+
+    return 0;
+}
