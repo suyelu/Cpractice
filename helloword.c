7e888156937b4d97e617a54c63d6bf978c411849
suyelu <suyelu@haizeix.com>
2019-07-21 14:50:20 +0800

    add file helloworld.c

---
+++ helloword.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: helloword.c
+	> Author: suyelu
+	> Mail: suyelu@haizeix.com
+	> Created Time: Sun 21 Jul 2019 02:48:56 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello World!\n");
+    return 0;
+}
+
